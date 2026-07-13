f24f3cbdb4d9148e301ae71d64f61021987c7a84
huojian17-star <huojian17@gmail.com>
2026-07-13 09:23:07 +0800

    Refactor API calls to fetch upstat data

--- api/bili-stats.js
+++ api/bili-stats.js
@@ -3,15 +3,20 @@ module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   try {
-    const [fansRes, playsRes] = await Promise.all([
+    const [fansRes, upstatRes] = await Promise.all([
       fetch('https://api.bilibili.com/x/relation/stat?vmid=' + UID),
       fetch('https://api.bilibili.com/x/space/upstat?mid=' + UID)
     ]);
     const fansData = await fansRes.json();
-    const playsData = await playsRes.json();
+    const upstatData = await upstatRes.json();
+    var totalPlays = null;
+    if (upstatData && upstatData.data) {
+      totalPlays = (upstatData.data.archive && upstatData.data.archive.view)
+                || upstatData.data.view || null;
+    }
     res.json({
       fans: fansData && fansData.data ? fansData.data.follower : null,
-      totalPlays: playsData && playsData.data && playsData.data.archive ? playsData.data.archive.view : null
+      totalPlays: totalPlays
     });
   } catch (e) {
     res.json({ fans: null, totalPlays: null });
